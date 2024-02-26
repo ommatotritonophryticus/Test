9b74eac7311254a58c544dde9c9382fa98be2f73
Some_Shit_Creator <campanula_barbata@riseup.net>
2024-02-26 09:21:12 +0100

    add custom walker_nav_menu class

---
+++ wordpress/wp-content/plugins/test-navbar/test-navbar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Plugin Name: test_navbar
+* Plugin URI: https://example.com
+* Description: navbar
+* Version: 1.0.0
+* Author: ...
+* Author URI: https://example.com
+* License: GPL2
+*/
+
+//use classes\classNewWalkerNavMenu\Test_Walker_Nav_Menu;
+
+require(__DIR__ . "/classes/class-new-walker-nav-menu.php");
+
+add_theme_support( 'menus' );
